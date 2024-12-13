46bb96cbed43586c7ba8c941ead0824c2466a605
NamekujiLSDs <okosimasu3@gmail.com>
2024-12-13 20:56:10 +0900

    modified:   assets/js/setting.js

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -20,12 +20,32 @@ module.exports = {
             localFile: 'Local File',
         },
     },
-    crosshairType: {
+    crosshairUrl: {
         id: 'crosshairUrlText',
         title: 'Crosshair URL',
         cat: 'Crosshair',
         type: 'text',
-        val: config.get('crosshairUrl', 'url'),
+        val: config.get('crosshairUrl', 'https://namekujilsds.github.io/CROSSHAIR/img/Cross-lime.png'),
     },
-
+    crosshairFile: {
+        id: 'crosshairLocalFile',
+        title: 'Crosshair File',
+        cat: 'Crosshair',
+        type: 'file',
+        val: config.get('crosshairUrl', ''),
+    },
+    crosshairFile: {
+        id: 'crosshairLocalFile',
+        title: 'Crosshair File',
+        cat: 'Crosshair',
+        type: 'file',
+        val: config.get('crosshairFilePath', ''),
+    },
+    crosshairFilePath: {
+        id: 'crosshairLocalFile',
+        title: 'Crosshair File Path',
+        cat: 'Crosshair',
+        type: 'display',
+        val: config.get('crosshairFilePath', ''),
+    },
 }
